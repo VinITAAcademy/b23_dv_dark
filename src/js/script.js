421530de2e03280ab2db757b12ba3d42162db81d
Yevhenii <evhenii.rybin@gmail.com>
2024-01-12 17:32:45 +0200

    partners' logos block added

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 // start modal burger menu
 
 //end modal burger menu
+
+//start logo of partners
+$(".owl-carousel").owlCarousel({
+    items: 2,
+    nav: true,
+    loop: true,
+    navText: ["<div class='icon-arrow-left'></div>","<div class='icon-arrow-right'></div>"]
+});
+//end logo of partners
