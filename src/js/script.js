42602a5edef019c01d4d29a1f938c80ac98e105f
Yevhenii <evhenii.rybin@gmail.com>
2024-01-12 21:50:58 +0200

    video poster added to 'we provide' section

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,46 @@
 
 //end modal burger menu
 
+
+//start we provide video poster
+/* $('.slick-test').slick(); */
+
+
+var videos = $(".video");
+
+videos.on("click", function () {
+    var elm = $(this),
+        conts = elm.contents(),
+        le = conts.length,
+        ifr = null;
+
+    for (var i = 0; i < le; i++) {
+        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
+    }
+
+    elm.addClass("player").html(ifr);
+    elm.off("click");
+});
+
+var videos2 = $(".video_with_autoplay");
+videos2.on("click", function () {
+    var elm = $(this),
+        conts = elm.contents(),
+        le = conts.length,
+        ifr = null;
+
+    for (var i = 0; i < le; i++) {
+        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
+    }
+
+    elm.addClass("player").html(ifr);
+    elm.off("click");
+    $("#video1")[0].src += "&autoplay=1";
+    
+});
+//start we provide video poster
+
+
 //start logo of partners
 $(".owl-carousel").owlCarousel({
     items: 2,
