f58c4f1b0a385b02fb28dd7329db956f9b0d8479
DariaShkolnik <dashkkashkolnik.gmail.com>
2024-01-13 00:02:48 +0200

    feat: add applicant modal with basic fields

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,61 @@ document.addEventListener('DOMContentLoaded', function () {
 // start modal burger menu
 
 //end modal burger menu
+
+// start applicant form
+
+const validationSchema = {
+    firstname: [
+      { pattern: /.+/, message: 'This field is required'},
+      { pattern: /^.{4,39}$/, message: 'Firstname should has 4-39 chars'},
+    ],
+    surname: [
+      { pattern: /.+/, message:  'This field is required' },
+      { pattern: /^.{4,39}$/, message: 'Firstname should has 4-39 chars'},
+    ],
+    email: [
+      { pattern: /.+/, message:  'This field is required' },
+      { pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message:  'Should be a valid email' }
+    ],
+    phone: [
+      {pattern: /.+/, message:  'This field is required' },
+      {pattern: /^(\+380|380|0)\d{9}$/, message:  'Should be a valid phone number' }
+    ],
+  };
+
+  const validate = (v, name) => {
+    const entity = validationSchema[name];
+
+    let isValid = true;
+    let errorText = '';
+
+    entity.forEach(rule => {const { pattern, message } = rule;
+          
+        isValid = !!pattern.test(v);
+        errorText = message;
+      });
+
+      return { isValid, message: errorText };
+    }
+
+    const formControls = document.querySelectorAll('#applicant_form_modal .applicant_form_modal_container .modal-content form .form_control');
+
+    formControls.forEach((control) => {
+      const input = control.querySelector('input');
+      const errorMessage = control.querySelector('.error_message');
+
+      input.addEventListener('input',  () => {
+        const { isValid, message } = validate(input.value, input.name);
+
+
+        if (!isValid) {
+          errorMessage.classList.add('active');
+          errorMessage.innerHTML = message;
+        } else {
+          errorMessage.classList.remove('active');
+        }
+      });
+    });
+
+// end applicant form
+
