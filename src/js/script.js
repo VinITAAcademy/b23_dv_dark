359dfe2805bd00113916c85e12175a41fe142cb9
SergeyKorol1982 <giyuki@gmail.com>
2024-01-16 13:20:37 +0200

    header add

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,24 @@
+// start loader
+
+//imitation of loading. uncomment for test
+
+/*
+document.onreadystatechange = function (e) {
+    if (document.readyState === 'complete') {
+        let animationPreload = document.querySelectorAll('.loader-block')[0];
+        animationPreload.style.visibility = 'visible';
+        animationPreload.style.opacity = '1';
+
+        let timerId = setTimeout(function () {
+            animationPreload.style.opacity = '0';
+            animationPreload.style.visibility = 'hidden';
+        }, 2000); 
+    }
+};
+*/
+
+// end loader
+
 // start specialities
 document.addEventListener('DOMContentLoaded', function () {
     let carouselBlock = document.querySelector('.specialty-carousel-main-container');
@@ -26,6 +47,52 @@ document.addEventListener('DOMContentLoaded', function () {
 // end specialities
 
 // start modal burger menu
+// end modal burger menu
+
+//start we provide video poster
+let videos = $(".video");
+
+videos.on("click", function () {
+    let elm = $(this),
+        conts = elm.contents(),
+        le = conts.length,
+        ifr = null;
+
+    for (let i = 0; i < le; i++) {
+        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
+    }
+
+    elm.addClass("player").html(ifr);
+    elm.off("click");
+});
+
+let videos2 = $(".video_with_autoplay");
+videos2.on("click", function () {
+    let elm = $(this),
+        conts = elm.contents(),
+        le = conts.length,
+        ifr = null;
+
+    for (let i = 0; i < le; i++) {
+        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
+    }
+
+    elm.addClass("player").html(ifr);
+    elm.off("click");
+    $("#we-provide-video")[0].src += "&autoplay=1";
+    
+});
+//end we provide video poster
+
+//start partners' logos
+$(".owl-carousel").owlCarousel({
+    items: 2,
+    nav: true,
+    loop: true,
+    navText: ["<div class='icon-arrow-left-logos'></div>","<div class='icon-arrow-right-logos'></div>"]
+});
+//end partners' logos
+// start modal burger menu
 $( document ).ready(function() {
 
     $( ".cross" ).hide();
@@ -46,48 +113,3 @@ $( document ).ready(function() {
     
     });
 // end modal burger menu
-
-//start we provide video poster
-let videos = $(".video");
-
-videos.on("click", function () {
-    let elm = $(this),
-        conts = elm.contents(),
-        le = conts.length,
-        ifr = null;
-
-    for (let i = 0; i < le; i++) {
-        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
-    }
-
-    elm.addClass("player").html(ifr);
-    elm.off("click");
-});
-
-var videos2 = $(".video_with_autoplay");
-videos2.on("click", function () {
-    let elm = $(this),
-        conts = elm.contents(),
-        le = conts.length,
-        ifr = null;
-
-    for (let i = 0; i < le; i++) {
-        if (conts[i].nodeType == 8) ifr = conts[i].textContent;
-    }
-
-    elm.addClass("player").html(ifr);
-    elm.off("click");
-    $("#video1")[0].src += "&autoplay=1";
-    
-});
-//end we provide video poster
-
-
-//start partners' logos
-$(".owl-carousel").owlCarousel({
-    items: 2,
-    nav: true,
-    loop: true,
-    navText: ["<div class='icon-arrow-left-logos'></div>","<div class='icon-arrow-right-logos'></div>"]
-});
-//end partners' logos
