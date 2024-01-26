b5b02e6e25e834166b99b3a3b87fbd5451c94ecc
SergeyKorol1982 <giyuki@gmail.com>
2024-01-26 14:20:46 +0200

    some bugs fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,71 +1,74 @@
 document.addEventListener('DOMContentLoaded', function () {
-// start specialities
-    let carouselBlock = document.querySelector('.specialty-carousel-main-container');
-    let carouselCardsContainer = document.querySelector('.specialty-carousel-main-container .specialty-carousel-cards-container');
-    let carouselCard = document.querySelectorAll('.carousel-item');
-    let screenWidth = window.screen.width;
-    
-    document.addEventListener('resize', function (e) {    
-        if (screenWidth >= 768) {
-            carouselBlock.removeAttribute('id');
-            carouselBlock.classList.remove('carousel');
-            carouselBlock.classList.remove('slide');
-            carouselCardsContainer.classList.remove('carousel-inner');
-            carouselCard[0].classList.remove('active');
-            for (let item of carouselCard) item.classList.remove('carousel-item');
-        } else if (screenWidth < 768) {
-                carouselBlock.setAttribute('id', 'carouselExampleIndicators');
-                carouselBlock.classList.add('carousel');
-                carouselBlock.classList.add('slide');
-                carouselCardsContainer.classList.add('carousel-inner');
-                carouselCard[0].classList.add('active');
-                for (let item of carouselCard) item.classList.add('carousel-item');
-        }
-    });
-// end specialities
+        // start specialities
+        let carouselBlock = document.querySelector('.specialty-carousel-main-container');
+        let carouselCardsContainer = document.querySelector('.specialty-carousel-main-container .specialty-carousel-cards-container');
+        let carouselCard = document.querySelectorAll('.carousel-item');
+        let screenWidth = window.screen.width;
 
-// top button start
-    const topButton = document.getElementById("top-button");
-    window.onscroll = function() {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            topButton.style.opacity = '1';
-        } else {
-            topButton.style.opacity = '0';
-        }
-    }
-
-    const iconArrow = document.getElementById("icon-arrow");
-    const buttonRect = iconArrow.getBoundingClientRect();
-    const whiteBlocks = document.querySelectorAll(".white-background");
-    const whiteCardsDesktop = document.querySelectorAll(".white-background-desktop");
-
-    const areIntersecting = (bounds1, bounds2) => 
-        bounds1.top < bounds2.bottom && bounds1.bottom > bounds2.top;
-
-    document.addEventListener('scroll', () => {
-
-        for (let item of whiteBlocks) {
-            const itemRect = item.getBoundingClientRect();
-            if (areIntersecting(itemRect, buttonRect)) {
-                iconArrow.classList.add("blue-button");
-                return; 
-            }
-            iconArrow.classList.remove("blue-button");
-        }
-
-        if (screenWidth >= 1024 && screenWidth < 1087) {
-            for (let item of whiteCardsDesktop) {
-                const itemRect = item.getBoundingClientRect();
-                if (areIntersecting(itemRect, buttonRect)) {
-                    iconArrow.classList.add("blue-button");
-                    return; 
+        document.addEventListener('resize', function (e) {
+                if (screenWidth >=768) {
+                    carouselBlock.removeAttribute('id');
+                    carouselBlock.classList.remove('carousel');
+                    carouselBlock.classList.remove('slide');
+                    carouselCardsContainer.classList.remove('carousel-inner');
+                    carouselCard[0].classList.remove('active');
+                    for (let item of carouselCard) item.classList.remove('carousel-item');
                 }
-                iconArrow.classList.remove("blue-button");
+
+                else if (screenWidth < 768) {
+                    carouselBlock.setAttribute('id', 'carouselExampleIndicators');
+                    carouselBlock.classList.add('carousel');
+                    carouselBlock.classList.add('slide');
+                    carouselCardsContainer.classList.add('carousel-inner');
+                    carouselCard[0].classList.add('active');
+                    for (let item of carouselCard) item.classList.add('carousel-item');
+                }});
+        // end specialities
+
+        // top button start
+        const topButton = document.getElementById("top-button");
+
+        window.onscroll = function() {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                topButton.style.opacity = '1';
             }
-        }
-        
-    });
-// top button end
+
+            else {
+                topButton.style.opacity = '0';
+            }}
+
+        const iconArrow = document.getElementById("icon-arrow");
+        const buttonRect = iconArrow.getBoundingClientRect();
+        const whiteBlocks = document.querySelectorAll(".white-background");
+        const whiteCardsDesktop = document.querySelectorAll(".white-background-desktop");
+
+        const areIntersecting = (bounds1, bounds2)=> bounds1.top < bounds2.bottom && bounds1.bottom > bounds2.top;
+
+        document.addEventListener('scroll', ()=> {
+
+                for (let item of whiteBlocks) {
+                    const itemRect = item.getBoundingClientRect();
+
+                    if (areIntersecting(itemRect, buttonRect)) {
+                        iconArrow.classList.add("blue-button");
+                        return;
+                    }
+
+                    iconArrow.classList.remove("blue-button");
+                }
+
+                if (screenWidth >=1024 && screenWidth < 1087) {
+                    for (let item of whiteCardsDesktop) {
+                        const itemRect = item.getBoundingClientRect();
+
+                        if (areIntersecting(itemRect, buttonRect)) {
+                            iconArrow.classList.add("blue-button");
+                            return;
+                        }
+
+                        iconArrow.classList.remove("blue-button");
+                    }}});
+        // top button end
 
         // start applicant form
         const applicantForm = $("#applicant_form_modal .form");
@@ -85,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let videos = $(".video");
 
         videos.on("click", function () {
-                let elm = $(this),
+                let elm=$(this),
                 conts = elm.contents(),
                 le = conts.length,
                 ifr = null;
@@ -98,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 elm.off("click");
             });
 
-        let videos2=$(".video_with_autoplay");
+        let videos2 = $(".video_with_autoplay");
 
         videos2.on("click", function () {
                 let elm = $(this),
@@ -107,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 ifr = null;
 
                 for (let i=0; i < le; i++) {
-                    if (conts[i].nodeType == 8) ifr = conts[i].textContent;
+                    if (conts[i].nodeType == 8) ifr=conts[i].textContent;
                 }
 
                 elm.addClass("player").html(ifr);
@@ -123,10 +126,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 nav: true,
                 loop: true,
                 navText: ["<div class='icon-arrow-left-logos'></div>", "<div class='icon-arrow-right-logos'></div>"]
-            });
-        //end partners' logos
+            });    
 
-    });
+    }
+
+);
+//end partners' logos
 // start modal burger menu
 
 let hamburger = document.querySelector('.hamburger');
@@ -148,6 +153,8 @@ closes.onclick=function () {
 
 // start modal selection menu
 let registration = document.querySelector('.hero_content_button');
+let registration1 = document.querySelector('.registration-content-button');
+let join = document.querySelector('.partners-button');
 let selection = document.querySelector('#selection-menu');
 let closeSelection = document.querySelector('.sm-cross');
 
@@ -159,6 +166,16 @@ registration.onclick = function() {
 closeSelection.onclick = function() {
     selection.style.display = 'none';
     hamburger.style.display = 'block';
+}
+
+registration1.onclick = function() {
+    selection.style.display = 'block';
+    hamburger.style.display = 'none';
+}
+
+join.onclick=function() {
+    selection.style.display = 'block';
+    hamburger.style.display = 'none';
 }
 
 // start modal selection menu
@@ -187,9 +204,26 @@ participant.onclick = function() {
 
 mentor.onclick = function() {
     selection.style.display = 'none';
-    mentorModal.style.opacity = '1';
+    mentorModal.style.opacity ='1';
     mentorModal.style.paddingTop ='57px';
     mentorModal.style.display = 'block';
 }
+
+let=modalCloseParticipant = document.querySelector('.modal_close-participant');
+let=modalClosePartner = document.querySelector('.modal_close-partner');
+let=modalCloseMentor = document.querySelector('.modal_close-mentor');
+
+modalCloseParticipant.onclick = function() {
+    participantModal.style.display = 'none';
+}
+
+modalClosePartner.onclick = function() {
+    partnerModal.style.display = 'none';
+}
+
+modalCloseMentor.onclick = function() {
+    mentorModal.style.display = 'none';
+}
+
 
 // end modal poll menu
